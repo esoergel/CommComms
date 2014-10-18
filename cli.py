41b501f1af070f7be4112501503c1588da7fdb57
Nick Pellegrino <npellegrino@dimagi.com>
2014-10-17 23:12:32 -0400

    add CLI mocks

---
+++ cli.py
@@ -0,0 +1,14 @@
+def login(username):
+    print "user %s is logged in" % username
+
+
+def logout(username):
+    print "user %s is logged out" % username
+
+
+def send(message):
+    print "sending message:\n%s" % message
+
+
+def receive(message):
+    print "received message:\n%s" % message
