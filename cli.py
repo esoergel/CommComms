38f342c2c85120efadab2f265dbd3a7e225e589e
Nick Pellegrino <npellegrino@dimagi.com>
2014-10-17 23:47:10 -0400

    add login view

--- cli.py
+++ cli.py
@@ -1,6 +1,10 @@
+import requests
+
+
+
 def login(username):
     print "user %s is logged in" % username
-
+    print requests.post('http://127.0.0.1:5000/login/', data={'username': username}).text
 
 def logout(username):
     print "user %s is logged out" % username
@@ -12,3 +16,10 @@ def send(message):
 
 def receive(message):
     print "received message:\n%s" % message
+
+
+if __name__ == '__main__':
+    login('esoergel')
+    send('first message')
+    receive('receiving message')
+    logout('esoergel')
--- commcomms.py
+++ commcomms.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!\n'
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    return "logging in user %s" % request.form['username']
 
 if __name__ == '__main__':
     app.run()
